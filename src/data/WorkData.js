8512acfc9cf7e758ea45367bdca807ce93fc1d2c
Ajay Irkal <Ajayirkal.mercy@gmail.com>
2022-08-29 04:17:40 +0530

    Update

---
+++ src/data/WorkData.js
@@ -0,0 +1,14 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Ecommerce Website (Kosha)",
+        description:"It is build on top of the React JS, Node, Express, MongoDb with styledComponents.",
+        tags:["react","Node","Express","MongoDB","styledComponents"],
+        
+        demo:"",
+        github:""
+    }
+  
+] 
